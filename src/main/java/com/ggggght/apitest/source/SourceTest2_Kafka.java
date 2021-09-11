b4ae22e291f342af846f4539764f4d8a1abd6de2
wangzheng <wangzheng@meishubao.com>
2021-09-11 18:50:12 +0800

    feat: from kafka

---
+++ src/main/java/com/ggggght/apitest/source/SourceTest2_Kafka.java
@@ -0,0 +1,16 @@
+package com.ggggght.apitest.source;
+
+import com.ggggght.bean.SensorReading;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+
+public class SourceTest2_Kafka {
+	public static void main(String[] args) throws Exception {
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+	}
+}
