8b98e24dc3700927fd7f7d88cb666759ae573bd5
GGGGGHT <wz9712203617@gmail.com>
2021-09-12 18:14:49 +0800

    fix: from flink error

--- src/main/java/com/ggggght/apitest/source/SourceTest2_Kafka.java
+++ src/main/java/com/ggggght/apitest/source/SourceTest2_Kafka.java
@@ -4,6 +4,7 @@ import com.ggggght.bean.SensorReading;
 import java.util.Properties;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import java.time.LocalDateTime;
@@ -17,10 +18,14 @@ public class SourceTest2_Kafka {
 		final Properties properties = new Properties();
 		properties.setProperty("bootstrap.servers", "localhost:9092");
 		properties.setProperty("group.id", "consumer-group");
-		properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-		properties.setProperty("auto.offset.reset", "latest");
+		//properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		//properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		//properties.setProperty("auto.offset.reset", "latest");
 
-		env.addSource(new FlinkKafkaConsumer<String>("sensor", new SimpleStringSchema(), properties));
+		DataStream<String> dataSource = env.addSource(
+				new FlinkKafkaConsumer<String>("sensor", new SimpleStringSchema(), properties));
+
+		dataSource.print();
+		env.execute();
 	}
 }
