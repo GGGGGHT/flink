13c8f8186b19d62cf0e89dbde41e3b816da14c53
GGGGGHT <wz9712203617@gmail.com>
2021-09-12 17:49:17 +0800

    feat: read from kafka

--- src/main/java/com/ggggght/apitest/source/SourceTest2_Kafka.java
+++ src/main/java/com/ggggght/apitest/source/SourceTest2_Kafka.java
@@ -1,16 +1,26 @@
 package com.ggggght.apitest.source;
 
 import com.ggggght.bean.SensorReading;
+import java.util.Properties;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.List;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
 public class SourceTest2_Kafka {
 	public static void main(String[] args) throws Exception {
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		final Properties properties = new Properties();
+		properties.setProperty("bootstrap.servers", "localhost:9092");
+		properties.setProperty("group.id", "consumer-group");
+		properties.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		properties.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		properties.setProperty("auto.offset.reset", "latest");
 
+		env.addSource(new FlinkKafkaConsumer<String>("sensor", new SimpleStringSchema(), properties));
 	}
 }
